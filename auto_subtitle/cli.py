dc57abf252fc7b5445ed6b8d91cfc60d7d0becf2
ByMarcos <77577228+ByMarcosDesigns@users.noreply.github.com>
2024-08-30 13:35:15 +0200

    Update cli.py

--- auto_subtitle/cli.py
+++ auto_subtitle/cli.py
@@ -7,7 +7,7 @@ import tempfile
 from typing import Iterator, TextIO, List
 from .utils import filename, str2bool, format_timestamp
 
-srt_path = ''
+srt_path = 'D:\Programacion\Github\AutoVideo\subtitled_video'
 
 def main():
     parser = argparse.ArgumentParser(
